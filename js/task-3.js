0177c869f15802a80517ae6b12117c1156593ad9
87stal <87stal@gmail.com>
2020-04-26 21:55:01 +0300

    Ready home work

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+const findLongestWord = function(string) {
+  const words = string.split(' ');
+  let longest = words[0];
+  for (let i = 0; i < words.length; i += 1) {
+    if (words[i].length > longest.length) {
+      longest = words[i];
+    }
+  }
+  return longest;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
